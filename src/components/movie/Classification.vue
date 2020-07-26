c1b9248bea284d65a0733cbf1c4d8b32062c7be8
Mark Honeychurch <mark@honeychurch.org>
2020-07-27 01:13:10 +1200

    Cleaning

--- src/components/movie/Classification.vue
+++ src/components/movie/Classification.vue
@@ -2,8 +2,8 @@
 	<g-link
 		:to="value.path"
 		:title="'See all movies classified ' + value.title"
-		style="font-family: Garamond, 'Times New Roman', serif;"
 		class="px-4 border-4 rounded-lg font-serif font-bold uppercase whitespace-no-wrap hover:no-underline"
+		style="font-family: Garamond, 'Times New Roman', serif;"
 	>
 		{{value.title}}
 	</g-link>
