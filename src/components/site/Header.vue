08572d8dff4e5cd365046676446c7d25b92af29d
Mark Honeychurch <mark.honeychurch@telesmart.co.nz>
2020-08-04 12:04:55 +1200

    Happy accident - bg as header bg.

--- src/components/site/Header.vue
+++ src/components/site/Header.vue
@@ -1,6 +1,7 @@
 <template>
 	<header
-		class="flex flex-col sm:flex-row pt-0 text-center bg-grey-lightest dark:bg-primary-dark text-black dark:text-grey-lightest bg-repeat-x leading-none transition-all duration-500 ease-in-out bg-bottom"
+		class="flex flex-col sm:flex-row pt-0 text-center bg-grey-lightest dark:bg-grey-darkest text-black dark:text-grey-lightest bg-repeat-x leading-none transition-all duration-500 ease-in-out bg-bottom"
+		style="--bg-opacity: 0.8;"
 		:style="style"
 		:class="{'p-6 pt-8': $store.state.top}"
 	><!-- style="background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.05), rgba(0, 0, 0, 0.05));" -->
@@ -37,7 +38,7 @@
 		},
 		computed: {
 			style() {
-				if (this.$store.state.dark) return {backgroundImage: `url('/img/backgrounds/curtain.png')`};
+				// if (this.$store.state.dark) return {backgroundImage: `url('/img/backgrounds/curtain.png')`};
 			},
 		},
 	}
